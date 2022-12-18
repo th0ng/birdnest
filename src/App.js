c08952573189e216d65fce58bc08b1836af224e3
Thongzxje <thonghoangdanh@gmail.com>
2022-12-18 22:32:35 +0200

    footer small fix

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { Header, Footer } from "./components";
 
-function App() {
+const App = () => {
   return (
     <>
       <Header />
       <Footer />
     </>
   );
-}
+};
 
 export default App;
