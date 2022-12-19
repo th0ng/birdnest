c5270ea32d9a2a57b0afe294ed569d1098556139
Thongzxje <thonghoangdanh@gmail.com>
2022-12-19 18:28:13 +0200

    small add

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Helmet } from "react-helmet";
 import { useState } from "react";
 import { Header, Footer } from "./components";
 import droneService from "./services/drones";
@@ -8,15 +9,19 @@ const App = () => {
   const [drones, setDrones] = useState([]);
   const hook = () => {
     droneService.getdronesPosition().then((information) => {setDrones(information);
-    console.log(information);
     }).catch((error) => console.log(error));
   }
   setInterval(() => {
     hook();
-  }, 2000);
+    console.log(drones);
+  }, 3000);
 
   return (
     <>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Birdnest</title>
+      </Helmet>
       <Header />
       <Footer />
     </>
