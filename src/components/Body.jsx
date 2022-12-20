b86bea6b0123d17d838b27130a443562d28388a0
Thongzxje <thonghoangdanh@gmail.com>
2022-12-20 20:32:02 +0200

    logic

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,21 +1,29 @@
 import React from 'react';
 import { useState } from 'react';
+import Radar from './Radar';
 
 const Body = ({ drones }) => {
   console.log(drones)
   const [device, setDevice] = useState(drones.children[0]);
+   const positionsData = drones.children[1].children;
+
   return (
-    <div className='flex flex-col w-full mb-5'>
-      <h2 className='font-bold font-mono text-xl text-indigo-500'>Device:</h2>
-      <p className='font-mono font-medium text-base'>
-            Device ID: {device.attributes.deviceId}
-            <br />
-            Started Time: {
-            new Date(device.children[1].value).toTimeString()
-            + ' on '
-            + new Date(device.children[1].value).toLocaleDateString()
-            }
-          </p>
+    <div className='flex w-full mb-5'>
+      <div className='flex flex-col w-1/2'>
+        <h2 className='font-bold font-mono text-xl text-indigo-500'>Device:</h2>
+        <p className='font-mono font-medium text-base'>
+        Device ID: {device.attributes.deviceId}
+        <br />
+        Started Time: {
+          new Date(device.children[1].value).toTimeString()
+          + ' on '
+          + new Date(device.children[1].value).toLocaleDateString()
+        }
+        </p>
+      </div>
+      <div className='flex w-1/2'>
+        <Radar data={positionsData}/>
+      </div>
     </div>
   );
 }
---
+++ src/components/Radar.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Radar = ({ data }) => {
+  console.log(data);
+  return (
+    <svg version="1" xmlns="http://www.w3.org/2000/svg" width="450" height="450"></svg>
+  )
+}
+
+export default Radar;
