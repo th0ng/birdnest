a32ef260a6c638b89184e7dca07b63fcdc3e87f0
Thongzxje <thonghoangdanh@gmail.com>
2022-12-20 20:41:41 +0200

    table component

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import { useState } from 'react';
 import Radar from './Radar';
+import Table from './Table';
 
 const Body = ({ drones }) => {
   console.log(drones)
   const [device, setDevice] = useState(drones.children[0]);
-   const positionsData = drones.children[1].children;
+  const positionsData = drones.children[1].children;
 
   return (
-    <div className='flex w-full mb-5'>
+    <>
+    <div className='flex w-full mb-5 font-mono'>
       <div className='flex flex-col w-1/2'>
-        <h2 className='font-bold font-mono text-xl text-indigo-500'>Device:</h2>
-        <p className='font-mono font-medium text-base'>
+        <h2 className='font-bold text-xl text-indigo-500'>Device:</h2>
+        <p className='font-medium text-base'>
         Device ID: {device.attributes.deviceId}
         <br />
         Started Time: {
@@ -25,6 +27,10 @@ const Body = ({ drones }) => {
         <Radar data={positionsData}/>
       </div>
     </div>
+    <div className='flex w-full mt-5 font-mono'>
+      <Table />
+    </div>
+    </>
   );
 }
 
---
+++ src/components/Table.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Table = ({ data }) => {
+  return (
+    <div>Table</div>
+  )
+}
+
+export default Table
