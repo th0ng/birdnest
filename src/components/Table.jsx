5873c8e0991378f0c2ed9748bf9c6a89c1d641a4
Thongzxje <thonghoangdanh@gmail.com>
2022-12-21 19:33:37 +0200

    small change

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -2,18 +2,29 @@ import React from 'react';
 import { useState } from 'react';
 import birdnestService from '../services/birdnest';
 
+//constructor for data to show in the table
 const dataConstructor = (serialNumber, positionX, positionY ) => {
   this.serialNumber = serialNumber;
   this.positionX = positionX;
   this.positionY = positionY;
   this.distance = Math.hypot(positionX, positionY);
-}
+};
 
+//function to push the data into array, with timestamp
+const push = (array, value) => {
+  array.push({
+     value: value,
+     time: Date.now()
+  });
+};
+
+//table function to be exported
 const Table = ({ data }) => {
   const [tableData, setTableData] = useState([]);
+
   return (
     <div>Table</div>
   )
-}
+};
 
-export default Table
+export default Table;
